2eef32432c5c52baab515715fe2409e305e2ae84
Ikaro Felix <ikaroisdm@gmail.com>
2023-04-01 11:37:36 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,13 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // Add this line to copy the _redirects file to the build output
+        _redirects: resolve(__dirname, "_redirects"),
+      },
+    },
+  },
 });
