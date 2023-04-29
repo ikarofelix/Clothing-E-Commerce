29c54a9e740d49d11d58fca59dfbc9d171a781a6
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-04-29 12:05:02 -0300

    Stripe
    Stripe implementation, alongside with serverless function and environment variables

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,6 +6,8 @@ import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkou
 
 import { selectCartTotal, selectCartItems } from "../../store/cart/cart.selector";
 
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
@@ -33,6 +35,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>TOTAL: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
